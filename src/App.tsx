142fff5a4f926395a76fc61bb74e416a0dface4f
lucas_pardinho <lucass706@hotmail.com>
2021-02-23 21:05:43 -0300

    adding routes for aplication

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './styles/global.css';
-import Home  from './components/pages/home/index';
+import Routes from './routes';
 
 function App() {
   return (
     <div>
-      <Home />
-      <h1>Hello word</h1>
-      <h2>Hello 2</h2>
+      <Routes />
+      
     </div>
   );
 }
